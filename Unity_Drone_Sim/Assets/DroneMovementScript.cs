62f36ed32ea2488b62e5d2fec1bcd0af6e613a02
TomazRak <rak.tomaz@gmail.com>
2018-05-20 19:52:07 +0200

    Add Drone

---
+++ Unity_Drone_Sim/Assets/DroneMovementScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovementScript : MonoBehaviour {
+
+    Rigidbody ourDrone;
+    public float upForce;
+
+    public void Awake()
+    {
+        ourDrone = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        MovementUpDown();
+
+        ourDrone.AddRelativeForce(Vector3.up * upForce);
+    }
+    
+    public void MovementUpDown()
+    {
+
+    }
+
+    /*// Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}*/
+}
