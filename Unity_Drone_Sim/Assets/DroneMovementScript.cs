7e0215252c631d609815782c61ac487758ce3c86
TomazRak <rak.tomaz@gmail.com>
2018-05-23 12:58:47 +0200

    Fly river

--- Unity_Drone_Sim/Assets/DroneMovementScript.cs
+++ Unity_Drone_Sim/Assets/DroneMovementScript.cs
@@ -7,10 +7,13 @@ public class DroneMovementScript : MonoBehaviour {
     public Transform[] cilj;
     public float hitrost;
 
+
     private int trenuten;
+    public List<Vector3> trajektorji = new List<Vector3>();
+    //trajektorji.Add(new Vector3(-1.48, 0, -1.54));
+    
 
-
-    /*Rigidbody ourDrone;
+    Rigidbody ourDrone;
     public float upForce;
 
     public void Awake()
@@ -27,25 +30,29 @@ public class DroneMovementScript : MonoBehaviour {
     
     public void MovementUpDown()
     {
-
-    }*/
+        
+    }
 
     ///*// Use this for initialization
     void Start () {
-		
-	}
+        //trajektorji.Add(new Vector3((float)-1.48, 0, (float)-1.54));
+        //trajektorji.Add(new Vector3((float)-1.48, 0, (float)2.94));
+        //trajektorji.Add(new Vector3((float)3.23, (float)1.4, (float)4.04));
+        //trajektorji.Add(new Vector3((float)3.66, 0, (float)-0.311));
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if(transform.position != cilj[trenuten].position)
+		if(transform.position != trajektorji[trenuten])
         {
-            Vector3 pos = Vector3.MoveTowards(transform.position, cilj[trenuten].position, hitrost * Time.deltaTime);
+            //Vector3 pos = Vector3.MoveTowards(transform.position, cilj[trenuten].position, hitrost * Time.deltaTime);
+            Vector3 pos = Vector3.MoveTowards(transform.position, trajektorji[trenuten], hitrost * Time.deltaTime);
             GetComponent<Rigidbody>().MovePosition(pos);
 
         }
         else
         {
-            trenuten = (trenuten + 1) % cilj.Length;
+            trenuten = (trenuten + 1) % trajektorji.Count;
         }
 	}//*/
 }
